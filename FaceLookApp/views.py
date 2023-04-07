6a3ea59e4984ee88906a4e715a9c2f9ad8db37a0
Facundo Lopez <fl492279@gmail.com>
2023-04-07 16:49:39 -0300

    Se declararon las funciones de cada pagina

--- FaceLookApp/views.py
+++ FaceLookApp/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def home(request):
+
+    return HttpResponse("home")
+
+def servicios(request):
+
+    return HttpResponse("servicios")
+
+def tienda(request):
+
+    return HttpResponse("tienda")
+
+def blog(request):
+
+    return HttpResponse("blog")
+
+def contacto(request):
+
+    return HttpResponse("coantacto")
