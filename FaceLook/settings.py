553445eca4ae40872385c259948dd9b4bbf5f3a7
Facundo Lopez <fl492279@gmail.com>
2023-04-11 12:13:25 -0300

    Se modificó para utilizar PostgreSQL

--- FaceLook/settings.py
+++ FaceLook/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'FaceLookApp',
+    'serviciosApp',
 ]
 
 MIDDLEWARE = [
@@ -74,13 +75,17 @@ WSGI_APPLICATION = 'FaceLook.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'faceloook_db',
+        'USER': 'postgres',
+        'PASSWORD' : '1234',
+        'HOST' : 'localhost',
+        'DATABASE_PORT' : '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
