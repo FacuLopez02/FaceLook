3acc9a75e60999b4bcbcf885e3b4ff241eb71875
Facundo Lopez <fl492279@gmail.com>
2023-04-12 09:46:05 -0300

    Se indicó la ruta donde se guardan las imagenes

--- serviciosApp/models.py
+++ serviciosApp/models.py
@@ -6,7 +6,7 @@ class Servicio(models.Model):
     
     titulo = models.CharField(max_length=50)
     contenido = models.CharField(max_length=50)
-    imagen = models.ImageField()
+    imagen = models.ImageField(upload_to='servicios') # Guarda las imagenes en una carpeta "servicios" (la crea si no existe)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now_add=True)
     
