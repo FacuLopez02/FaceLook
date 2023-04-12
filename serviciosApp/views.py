6a1eba77770b46dcff57fb722e87071c12f31ba5
Facundo Lopez <fl492279@gmail.com>
2023-04-12 11:43:43 -0300

    Se independizó la funcion servicios

--- serviciosApp/views.py
+++ serviciosApp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Servicio
 
 # Create your views here.
+
+def servicios(request):
+    list_servicios = Servicio.objects.all()
+    
+    return render(request,'servicios/servicios.html',{"servicios": list_servicios})
