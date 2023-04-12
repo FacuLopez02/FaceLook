1b0ac8e4d8be2af15141961370a669a28104d4b7
Facundo Lopez <fl492279@gmail.com>
2023-04-12 09:48:24 -0300

    Se indicó la ruta donde se guardan los archivos multimedia

--- FaceLookApp/urls.py
+++ FaceLookApp/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from FaceLookApp import views
 
+# Importaciones para encontrar los archivos multimedia
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('', views.home, name="Home"),
     path('servicios/', views.servicios, name="Servicios"),
@@ -8,3 +12,6 @@ urlpatterns = [
     path('blog/', views.blog, name="Blog"),
     path('contacto/', views.contacto, name="Contacto"),
 ]
+
+# Ruta para encontrar los archivos multimedia
+urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
