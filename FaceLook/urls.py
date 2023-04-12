cbdad0ce653d6eaee7ccf46cd5216661aa5a7110
Facundo Lopez <fl492279@gmail.com>
2023-04-12 11:39:40 -0300

    Se incluyó el URL de servicios

--- FaceLook/urls.py
+++ FaceLook/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('FaceLookApp.urls')),
+    path('servicios/',include('serviciosApp.urls'))
 ]
