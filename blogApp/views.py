a6a4b800436ba46bccda5ad01717ed81ada4d855
Facundo Lopez <fl492279@gmail.com>
2023-04-13 12:23:43 -0300

    Se agregó la funcion para filtrar por categoria

--- blogApp/views.py
+++ blogApp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Categoria
 
 # Create your views here.
 
@@ -8,3 +8,11 @@ def blog(request):
     lista_posteos = Post.objects.all()
     
     return render(request,'blog/blog.html',{'posteos': lista_posteos})
+
+def categoria(request, categoria_id):
+    
+    categoria = Categoria.objects.get(id=categoria_id)
+    
+    lista_posteos = Post.objects.filter(categorias=categoria)
+    
+    return render(request, 'blog/categoria.html',{'categoria':categoria, 'posteos': lista_posteos})
