be0623e21003c843e5a0e0e4d903efdf0c34bf24
Facundo Lopez <fl492279@gmail.com>
2023-04-13 10:59:55 -0300

    Contiene las funciones del blog

--- blogApp/views.py
+++ blogApp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
+def blog(request):
+
+    lista_posteos = Post.objects.all()
+    
+    return render(request,'blog/blog.html',{'posteos': lista_posteos})
