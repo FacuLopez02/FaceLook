0eaedd701ed9e22d057fd7aee1cac6e7e2b2fcb9
Facundo Lopez <fl492279@gmail.com>
2023-04-13 10:43:08 -0300

    Se agregó la ruta de la APP blog

--- FaceLook/urls.py
+++ FaceLook/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('FaceLookApp.urls')),
-    path('servicios/',include('serviciosApp.urls'))
+    path('servicios/',include('serviciosApp.urls')),
+    path('blog/', include('blogApp.urls')),
 ]
