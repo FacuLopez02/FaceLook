d10c8744734110f50f8b584f34b2745cbc4923e9
Facundo Lopez <fl492279@gmail.com>
2023-04-19 11:38:14 -0300

    Se implementó la validacion del formulario y guardado de datos

--- ContactoApp/views.py
+++ ContactoApp/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
 from .forms import contacto_form
+from django.shortcuts import redirect
 
 # Create your views here.
 
 
 def contacto(request):
     formulario = contacto_form()
+    
+    if request.method=="POST":
+        
+        formulario = contacto_form(data = request.POST)
+        
+        if formulario.is_valid():
+            
+            nombre = request.POST.get("nombre")
+            email = request.POST.get("email")
+            contenido = request.POST.get("contenido")
+            
+            return redirect("/contacto/?valido")
+            
+    
 
     return render(request,'contacto/contacto.html',{'formulario':formulario})
