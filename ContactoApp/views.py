f9ca6e75a293ad4141f28fd2d3a253ce7b9f0e6f
Facundo Lopez <fl492279@gmail.com>
2023-04-20 10:31:01 -0300

    Se agregó la funcion para enviar mails

--- ContactoApp/views.py
+++ ContactoApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .forms import contacto_form
 from django.shortcuts import redirect
+from django.core.mail import send_mail
 
 # Create your views here.
 
@@ -17,8 +18,12 @@ def contacto(request):
             nombre = request.POST.get("nombre")
             email = request.POST.get("email")
             contenido = request.POST.get("contenido")
+            try:
+                send_mail("Mail de "+nombre, contenido + "\n\n" + "Responder a: " + email, 'fl492279@gmail.com', ['fl492279@gmail.com'])
+                return redirect("/contacto/?valido")
             
-            return redirect("/contacto/?valido")
+            except:
+                return redirect("/contacto/?no_valido")
             
     
 
