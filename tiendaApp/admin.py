59007580910a0ce42f388a37d31f534d1a7670cc
Facundo Lopez <fl492279@gmail.com>
2023-04-24 10:20:51 -0300

    Se incluyó la administracion de productos

--- tiendaApp/admin.py
+++ tiendaApp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import categoria_producto, producto
 
 # Register your models here.
+class categoria_prod_admin(admin.ModelAdmin):
+    
+    readonly_fields=('created','updated')
+    
+
+class prod_admin(admin.ModelAdmin):
+    
+    readonly_fields=('created','updated')
+
+
+admin.site.register(categoria_producto, categoria_prod_admin)
+admin.site.register(producto, prod_admin)
