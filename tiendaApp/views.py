1acd7f0115a07eae1d0fe4401070bedcd35c01e6
Facundo Lopez <fl492279@gmail.com>
2023-04-24 10:22:32 -0300

    Se agregó una lista que contiene todos los objetos productos

--- tiendaApp/views.py
+++ tiendaApp/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import producto
 
 # Create your views here.
 
 def tienda(request):
 
-    return render(request,'tienda/tienda.html')
+    lista_productos = producto.objects.all()
+    
+    return render(request,'tienda/tienda.html',{'productos': lista_productos})
