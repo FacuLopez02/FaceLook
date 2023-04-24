b53f62b9963f4c820605d5a39e4f01e263e2bd48
Facundo Lopez <fl492279@gmail.com>
2023-04-24 10:21:46 -0300

    Se agregaron los campos updated y created en la tabla productos

--- tiendaApp/models.py
+++ tiendaApp/models.py
@@ -4,25 +4,30 @@ from django.db import models
 
 class categoria_producto(models.Model):
     
-    nombre_cat = models.CharField(max_length=50)
+    nombre = models.CharField(max_length=50)
     created = models.DateField(auto_now_add=True)
     updated = models.DateField(auto_now_add=True)
     
     class Meta:
-        verbose_name = "categoria_producto"
-        verbose_name_plural ="categorias_productos"
+        verbose_name = "categoriaProd"
+        verbose_name_plural ="categoriasProd"
         
-    def str(self):
+    def __str__(self):
         return self.nombre
     
 class producto(models.Model):
     
-    nombre_prod = models.CharField(max_length=50)
+    nombre = models.CharField(max_length=50)
     categorias = models.ForeignKey(categoria_producto, on_delete=models.CASCADE)
     imagen = models.ImageField(upload_to="tienda", null=True, blank=True)
     precio = models.FloatField()
     disponibilidad = models.BooleanField(default=True)
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateField(auto_now_add=True)
     
     class Meta:
         verbose_name = "producto"
         verbose_name_plural ="productos"
+        
+    def __str__(self):
+        return self.nombre
