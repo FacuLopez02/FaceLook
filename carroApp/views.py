9b0aec9f71d39f6fa1426e314bb786316a8ee523
Facundo Lopez <fl492279@gmail.com>
2023-04-25 09:55:28 -0300

    Se agregaron las funciones del carro

--- carroApp/views.py
+++ carroApp/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
+from .carro import Carro
+from tiendaApp.models import producto
+from django.shortcuts import redirect
 
 # Create your views here.
+
+def agregar_producto (request, producto_id):
+    
+    carro = Carro(request)
+    
+    producto_aux = producto.objects.get(id=producto_id)
+    
+    carro.agregar(producto_aux)
+    
+    return redirect("tienda")
+
+def eliminar_producto (request, producto_id):
+    
+    carro = Carro(request)
+    
+    producto_aux = producto.objects.get(id=producto_id)
+    
+    carro.eliminar(producto_aux)
+    
+    return redirect("tienda")
+
+def restar_producto (request, producto_id):
+    
+    carro = Carro(request)
+    
+    producto_aux = producto.objects.get(id=producto_id)
+    
+    carro.restar_producto(producto_aux)
+    
+    return redirect("tienda")
+
+def limpiar(request):
+    
+    carro = Carro(request)
+    
+    carro.limpiar_carro()
+    
+    return redirect("tienda")
