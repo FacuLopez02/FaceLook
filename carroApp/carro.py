e0a5e1c78447fb6f09f772767ab2b8a4bded3859
Facundo Lopez <fl492279@gmail.com>
2023-04-25 09:21:54 -0300

    Se agregaron las funciones del carro

--- carroApp/carro.py
+++ carroApp/carro.py
@@ -24,5 +24,41 @@ class Carro:
                 "imagen": producto.imagen.url,
                 }
         
+        else: 
+            for key, value in self.carro.items():
+                
+                if key == str(producto.id):
+                    
+                    value["cantidad"] += 1
+                    break
+                
+        self.guardar_carro()
+    
+    def eliminar(self, producto):
         
+        if str(producto.id) in self.carro:
+            del self.carro[str(producto.id)]
             
+        self.guardar_carro()
+        
+    def restar_producto(self, producto):
+        
+        for key, value in self.carro.items():
+                
+            if key == str(producto.id):
+                
+                value["cantidad"] -= 1
+                
+                if value["cantidad"] <= 0:
+                    self.eliminar(producto)
+                break
+            
+        self.guardar_carro()
+        
+    def limpiar_carro(self):
+        self.session["carro"] = {}
+        self.session.modified=True
+    
+    def guardar_carro(self):
+            self.session["cararo"] = self.carro
+            self.session.modified=True
