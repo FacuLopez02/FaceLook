d5fd81e0538b6719b454555a138d27ef75baf8eb
Facundo Lopez <fl492279@gmail.com>
2023-04-26 12:25:24 -0300

    Sin cambios

--- carroApp/context_processor.py
+++ carroApp/context_processor.py
@@ -2,10 +2,10 @@
 def importe_total(request):
     
     total = 0
-    if request.user.is_authenticated:
-        
-        for key, value in request.session["carro"].items():
+    #if request.user.is_authenticated:
+
+    for key, value in request.session["carro"].items():
             
-            total += float(value["precio"])*value["cantidad"]
-            
+        total += float(value["precio"]) 
+
     return {"importe_total": total} 
