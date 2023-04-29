54e4f0fc8c323025f083217483a0e3e6a01eeaa2
Facundo Lopez <fl492279@gmail.com>
2023-04-29 18:14:00 -0300

    Se agregaron las validaciones de logueo

--- autenticacionApp/views.py
+++ autenticacionApp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 
 # Create your views here.
@@ -31,3 +31,40 @@ class view_registro(View):
                 messages.error(request, form.error_messages[msg])
 
             return render(request, "registro/registro.html", {"form":form})
+
+def cerrar_sesion(request):
+        
+    logout(request)
+        
+    return redirect('Home') 
+
+def loguearse(request):
+
+    if request.method=="POST":
+
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+
+            nombre_usuario = form.cleaned_data.get("username")
+            contraseña = form.cleaned_data.get("password")
+
+            usuario = authenticate(username=nombre_usuario, password=contraseña)
+
+            if usuario is not None:
+
+                login(request, usuario)
+
+                return redirect('Home')
+            
+            else:
+                messages.error(request,"Usuario no válido.")
+
+        else:
+
+            messages.error(request,"Información incorrecta.")
+
+    form = AuthenticationForm()
+
+    return render(request, "login/login.html", {"form":form})
+
