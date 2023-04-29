feeb858539dfa427ac06b165b262cd0713107443
Facundo Lopez <fl492279@gmail.com>
2023-04-29 14:08:02 -0300

    Se agregó la funcion de logueo

--- autenticacionApp/views.py
+++ autenticacionApp/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib import messages
 
 # Create your views here.
 
@@ -13,4 +15,19 @@ class view_registro(View):
     
     def post(self, request):
         
-        pass
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+
+            usuario = form.save()
+            
+            login(request, usuario)
+
+            return redirect('Home')
+        
+        else:
+
+            for msg in form.error_messages:
+                messages.error(request, form.error_messages[msg])
+
+            return render(request, "registro/registro.html", {"form":form})
