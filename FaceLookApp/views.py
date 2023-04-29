bc36a628e068b230b979e1a533520a8e7c19fb8a
Facundo Lopez <fl492279@gmail.com>
2023-04-29 19:16:55 -0300

    Se incializó un carro

--- FaceLookApp/views.py
+++ FaceLookApp/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from carroApp.carro import Carro
 
 # Create your views here.
 
 def home(request):
+
+    carro = Carro(request)
 
     return render(request,'FaceLookApp/home.html')
