8ca2cf370a4426468536fe12d910576213058238
Facundo Lopez <fl492279@gmail.com>
2023-05-19 12:00:55 -0300

    Se agregaron listas nuevas para eliminar los filtros repetidos

--- blogApp/views.py
+++ blogApp/views.py
@@ -6,13 +6,15 @@ from .models import Post, Categoria
 def blog(request):
 
     lista_posteos = Post.objects.all()
-    
-    return render(request,'blog/blog.html',{'posteos': lista_posteos})
+    lista_categorias = Post.objects.all().distinct("categorias")
+
+    return render(request,'blog/blog.html',{'posteos': lista_posteos,'posteos_no_rep': lista_categorias})
 
 def categoria(request, categoria_id):
     
     categoria = Categoria.objects.get(id=categoria_id)
     
     lista_posteos = Post.objects.filter(categorias=categoria)
+    lista_categorias = Post.objects.filter(categorias=categoria).distinct("categorias")
     
-    return render(request, 'blog/categoria.html',{'categoria':categoria, 'posteos': lista_posteos})
+    return render(request, 'blog/categoria.html',{'categoria':categoria, 'posteos': lista_posteos, 'posteos_no_rep': lista_categorias})
